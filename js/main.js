ee7afbef1c80e014b7c7a870bbd99e86b3dab91a
Alisher <info@regname.uz>
2022-10-26 19:05:30 +0500

    finished

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
-$(function () {
-    // $('.slider').slick({
-       
-    // })
- })
+const formEl = document.querySelector(".consultation__form");
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const nameEl = formEl.querySelector("#name");
+  const phoneEl = formEl.querySelector("#phone");
+
+  const name = nameEl.value.trim();
+  const phone = phoneEl.value.trim();
+
+  if (name != "" && phone != "") {
+    const user = {
+      name,
+      phone,
+    };
+    sendData(user);
+    nameEl.textContent = "";
+    phoneEl.textContent = "";
+  } else {
+    alert("Заполните все поля");
+  }
+});
+
+function sendData({ name, phone }) {
+  alert(`Имя - ${name}, Номер телефона ${phone}`);
+}
